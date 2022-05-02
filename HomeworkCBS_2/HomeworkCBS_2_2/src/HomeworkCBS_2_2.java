d5bfd7337c1f765564603cd29f64df78bb58f6c6
Serhii <gatsserv@gmail.com>
2022-05-03 01:20:06 +0300

    Add second homework

---
+++ HomeworkCBS_2/HomeworkCBS_2_2/src/HomeworkCBS_2_2.java
@@ -0,0 +1,13 @@
+// Домашнее задание 2
+// Задание 2
+
+class HomeworkCBS_2_2 {
+    public static void main(String[] args) {
+        final double PI = 3.141592653;
+        final double E = 2.7182818284590452;
+
+        System.out.println("Пи= " + PI);
+        System.out.println("E= " + E);
+
+    }
+}
---
+++ HomeworkCBS_2/HomeworkCBS_2_4/src/HomeworkCBS_2_4.java
@@ -0,0 +1,54 @@
+//Домашнее задание 2
+//Задание 4
+
+public class HomeworkCBS_2_4 {
+    public static void main(String[] args) {
+
+        char ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch8, ch9, ch10, ch11, ch12, ch13, ch14, ch15, ch16, ch17, ch18, ch19, ch20, ch21, ch22;
+        ch1 = '\u0417';
+        ch2 = '\u0434';
+        ch3 = '\u0440';
+        ch4 = '\u0430';
+        ch5 = '\u0432';
+        ch6 = '\u0441';
+        ch7 = '\u0442';
+        ch8 = '\u0432';
+        ch9 = '\u0443';
+        ch10 = '\u0439';
+        ch11 = '\u0442';
+        ch12 = '\u0435';
+        ch13 = '\u002C';
+        ch14 = '\u0020';
+        ch15 = '\u0421';
+        ch16 = '\u0435';
+        ch17 = '\u0440';
+        ch18 = '\u0433';
+        ch19 = '\u0435';
+        ch20 = '\u0439';
+        ch21 = '\u0020';
+        ch22 = '\u0021';
+
+        System.out.print(ch1);
+        System.out.print(ch2);
+        System.out.print(ch3);
+        System.out.print(ch4);
+        System.out.print(ch5);
+        System.out.print(ch6);
+        System.out.print(ch7);
+        System.out.print(ch8);
+        System.out.print(ch9);
+        System.out.print(ch10);
+        System.out.print(ch11);
+        System.out.print(ch12);
+        System.out.print(ch13);
+        System.out.print(ch14);
+        System.out.print(ch15);
+        System.out.print(ch16);
+        System.out.print(ch17);
+        System.out.print(ch18);
+        System.out.print(ch19);
+        System.out.print(ch20);
+        System.out.print(ch21);
+        System.out.print(ch22);
+    }
+}
